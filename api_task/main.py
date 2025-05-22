2ae8d59d19d3754564f10ef73ae7d671e146508a
Igor Batischev <igor.bat2013@mail.ru>
2025-05-22 12:01:12 +0300

    Добавление роутера для crud операций

--- api_task/main.py
+++ api_task/main.py
@@ -1,9 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
 
+import api_task
+import api_task.routes
+import api_task.routes.task_routes
 from api_task.database import init_db
 
 app = FastAPI()
+
+app.include_router(api_task.routes.task_routes.router)
 
 
 @app.on_event("startup")
---
+++ api_task/routes/task_routes.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from api_task import crud
+from api_task.database import async_session
+from api_task.models import TaskCreate, TaskRead, TaskUpdate
+
+router = APIRouter()
+
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
+
+
+@router.post("/tasks/", response_model=TaskRead)
+async def create_task(task: TaskCreate, session: AsyncSession = Depends(get_session)):
+    return await crud.create_task(session, task)
+
+
+@router.get("/tasks/", response_model=list[TaskRead])
+async def get_tasks(session: AsyncSession = Depends(get_session)):
+    return await crud.get_all_tasks(session)
+
+
+@router.put("/tasks/{task_id}", response_model=TaskRead)
+async def update_task(
+    task_id: int, task: TaskUpdate, session: AsyncSession = Depends(get_session)
+):
+    result = await crud.update_task(session, task_id, task)
+    if not result:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return result
+
+
+@router.delete("/tasks/{task_id}")
+async def delete_task(task_id: int, session: AsyncSession = Depends(get_session)):
+    result = await crud.delete_task(session, task_id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"ok": True}
